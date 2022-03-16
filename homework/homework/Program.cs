e51a3d736cbdbc5533a0bcc9056cea0ba33fde4a
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-03-16 11:50:35 +0400

    Homework Completed

---
+++ homework/homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string username = "";
+            string password;
+            User user = new User(username);
+            do
+            {
+                Console.Write("Istifadeci adini daxil edin: ");
+                username = Console.ReadLine();
+                user.UserName = username;
+                if (user.UserName == null)
+                    Console.WriteLine("Istifadeci adi uzunluqu min 6 dan kicik max 25 den boyuk ola bilmez ");
+            } while (user.UserName == null);
+            do
+            {
+                Console.Write("Sifreni Daxil edin: ");
+                password = Console.ReadLine();
+                user.Password = password;
+                if (user.Password == null)
+                    Console.WriteLine("Sifrenin uzunluqu min 8 den kicik max 25 den boyuk ola bilmez ve daxilinde min 1 boyuk, min 1 kicik herf ve min 1 reqem olmalidir");
+            } while (user.Password == null);
+        }
+    }
+}
+    
---
+++ homework/homework/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+        public User(string username)
+        {
+            UserName = username;
+        }
+        public string UserName
+        {
+            get
+            {
+                return this._username;
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25 && !string.IsNullOrWhiteSpace(value))
+                    this._username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                if (value.Length >= 8 && value.Length <= 25 && HasDigit(value) && HasLower(value) && HasUpper(value) && !string.IsNullOrWhiteSpace(value))
+                    this._password = value;
+            }
+        }
+        public
+        static bool HasDigit(string pasword)
+        {
+            for (int i = 0; i < pasword.Length; i++)
+            {
+                if (char.IsDigit(pasword[i]))
+                    return true;
+            }
+
+            return false;
+        }
+        static bool HasLower(string pasword)
+        {
+            for (int i = 0; i < pasword.Length; i++)
+            {
+                if (char.IsLower(pasword[i]))
+                    return true;
+            }
+
+            return false;
+        }
+        static bool HasUpper(string pasword)
+        {
+            for (int i = 0; i < pasword.Length; i++)
+            {
+                if (char.IsUpper(pasword[i]))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
+
